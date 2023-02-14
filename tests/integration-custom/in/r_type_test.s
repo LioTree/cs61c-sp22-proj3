73f44d3c8cca79526d0f11dadb969124a90d52fc
LioTree <uc0110@163.com>
2023-02-15 00:22:26 +0800

    pass Task 6: R-type Instructions

---
+++ tests/integration-custom/in/r_type_test.s
@@ -0,0 +1,14 @@
+addi t0,x0,3
+addi t1,t0,2
+add t1,t0,t1
+sub t1,t1,t0
+and t1,t1,t0
+or t1,t1,t0
+xor t1,t1,t0
+sll t1,t1,t0
+srl t1,t1,t0
+sra t1,t1,t0
+slt t1,t1,t0
+mul t1,t1,t0
+mulh t1,t1,t0
+mulhu t1,t1,t0
